f813fa5a39f73849ee955c7ed4f2d8ca9f896da7
Cortez Christian <CortezChristian@rocketmortgage.com>
2021-10-19 16:12:30 -0400

    Final code submission

---
+++ DevBuildCircleLab/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevBuildCircleLab
+{
+    class Circle
+    {
+        private double radius = 0;
+        private double circumference => 2 * Math.PI * radius;
+        private double area => Math.PI * radius * radius;
+
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+        public double Circumference { get { return circumference; } }
+        public double Area { get { return area; } }
+
+        public Circle(double incomingRadius)
+        {
+            Radius = incomingRadius;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Radius: {Radius}\n" +
+                $"Circumference: {Circumference}\n" +
+                $"Area: {Area}\n";
+        }
+
+    }
+}
---
+++ DevBuildCircleLab/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevBuildCircleLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Circle> circles = new List<Circle>();
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                circles.Add(NewCircle());
+
+                if (!Continue())
+                {
+                    keepGoing = false;
+                    PrintCircles(circles);
+                }
+            }
+
+        }
+
+        public static Circle NewCircle()
+        {
+            Circle userCircle = new Circle(ValidRadius());
+            Console.WriteLine("Circle made\n");
+            return userCircle;
+        }
+
+        public static void PrintCircles(List<Circle> circles)
+        {
+            foreach(Circle circle in circles)
+            {
+                Console.WriteLine(circle);
+            }
+        }
+
+        public static double ValidRadius()
+        {
+
+            string userAttempt = GetUserInput("Please enter your radius: ");
+
+            try
+            {
+                double radius = double.Parse(userAttempt);
+                if ( radius > 0)
+                {
+                    return radius;
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid radius. Please enter a number greater than 0.");
+                    return ValidRadius();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("That was not a valid radius. Please enter a number greater than 0.");
+                return ValidRadius();
+            }
+
+            
+        }
+
+        public static string GetUserInput(string prompt)
+        {
+            Console.Write(prompt);
+            string output = Console.ReadLine().Trim().ToLower();
+            Console.WriteLine();
+
+
+            return output;
+        }
+
+        public static bool Continue()
+        {
+            string answer = GetUserInput("Continue? (y/n): ");
+
+            if (answer.ToLower().Trim() == "y")
+            {
+                return true;
+            }
+            else if (answer.ToLower().Trim() == "n")
+            {
+                Console.WriteLine("Here are your circles:");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Please enter either yes or no to continue.");
+                return Continue();
+            }
+
+        }
+    }
+
+    
+}
